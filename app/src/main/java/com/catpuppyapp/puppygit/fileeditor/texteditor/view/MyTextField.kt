bd46a4f43655904e787ae9a7b33b41e16af7c98a
bandeapart <dont.try.email.me@commit>
2025-07-30 18:57:57 +0800

    NO REVIEWED: gave up, can't solved: try resolve ime state loss after add closed pair

--- app/src/main/java/com/catpuppyapp/puppygit/fileeditor/texteditor/view/MyTextField.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/fileeditor/texteditor/view/MyTextField.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -26,11 +27,13 @@ import com.catpuppyapp.puppygit.fileeditor.texteditor.state.TextFieldState
 import com.catpuppyapp.puppygit.syntaxhighlight.base.PLFont
 import com.catpuppyapp.puppygit.syntaxhighlight.codeeditor.MyCodeEditor
 import com.catpuppyapp.puppygit.ui.theme.Theme
+import com.catpuppyapp.puppygit.utils.AppModel
+import com.catpuppyapp.puppygit.utils.MyLog
 
 typealias ComposeTextFieldState = androidx.compose.foundation.text.input.TextFieldState
 typealias ComposeTextFieldStateSaver = androidx.compose.foundation.text.input.TextFieldState.Saver
 
-//private const val TAG = "MyTextField"
+private const val TAG = "MyTextField"
 
 
 @Composable
@@ -51,6 +54,7 @@ internal fun MyTextField(
 ) {
     val focusRequester = remember { FocusRequester() }
     val textStyle = LocalTextStyle.current
+    val inDarkTheme = Theme.inDarkTheme
 
     //写法1
 //    val currentTextField = remember { mutableStateOf(textFieldState) }  //重组不会重新执行
@@ -58,66 +62,82 @@ internal fun MyTextField(
 
     //写法2，这种写法等同于写法1，之所以使用currentTextFiled的地方能获取到重组后最新的值是因为本质上by是state.value的语法糖，所以使用by后的值等于 获取 state.value ，所以value一旦更新，调用state.value就能获取到最新值
 //    val textState = textFieldState.value.let { rememberSaveable(it, saver = ComposeTextFieldStateSaver) { ComposeTextFieldState(it.text, it.selection) } }
-    val textState = textFieldState.value.let { remember(it) { ComposeTextFieldState(it.text, it.selection) } }
+    val textState = textFieldState.value.let { remember { mutableStateOf(ComposeTextFieldState(it.text, it.selection)) } }
 
 
-    val inDarkTheme = Theme.inDarkTheme
+
+    fun onValueChange(newState: TextFieldValue) {
+        val lastTextField = textFieldState.value
+
+
+        //存在选中文本时，显示光标拖手和背景颜色（handle）
+        needShowCursorHandle.value = newState.selection.start != newState.selection.end
+
+        // no change
+        if (lastTextField == newState) {
+            return
+        }
+
+        // used for some checks
+        val textChanged = lastTextField.text.length != newState.text.length || lastTextField.text != newState.text
+
+        // scroll if invisible
+        // when input some chars but target line invisible, will scroll to that line
+        if(textChanged) {
+            scrollIfInvisible()
+        }
+
+        if (newState.text.contains('\n')) {
+            onContainNewLine(newState)
+        } else {
+            // only update state when no linebreak,
+            //   if has line break, still update, will got unexpected line break,
+            //   for avoid it, just wait the text field list updated it, nothing need to do here
+
+            val newState = if(textChanged) {
+                newState
+            } else {
+                // copy to avoid lost highlighting styles when text no changes,
+                // if styles still lost, try use `textFieldState.value` as `lastTextField`
+                lastTextField.copy(selection = newState.selection)
+            }
+
+//            currentTextField.value = newState
+
+            onUpdateText(newState, textChanged)
+        }
+    }
+
+
 
     // if the `value` is not equals to `BasicTextField` held value, then the ime state will reset
     BasicTextField(
-        state = textState,
+        state = textState.value,
         inputTransformation = InputTransformation {
+            val newText = toString()
+            if(newText == textFieldState.value.text) {
+                onValueChange(textFieldState.value.copy(selection = selection))
+            }else {
+                onValueChange(TextFieldValue(newText, selection))
+            }
         },
         outputTransformation = OutputTransformation {
-            val textNoChange = toString() == textFieldState.value.text
-            if(textNoChange) {
+            val newText = toString()
+            // format output
+            if(newText.isNotEmpty()) {
+                val rangeOfNewText = TextRange(0, newText.length)
                 textFieldState.value.annotatedString.spanStyles.forEach {
-                    addStyle(it.item, it.start, it.end)
+                    // even change state, still keep styles before new style generated
+                    if(rangeOfNewText.contains(TextRange(it.start, it.end - 1))) {
+                        addStyle(it.item, it.start, it.end)
+                    }
                 }
             }
         },
 //        value = currentTextField.value,
         readOnly = readOnly,
         enabled = enabled,
-//        onValueChange = { newState ->
-//            val lastTextField = currentTextField.value
-//
-//
-//            //存在选中文本时，显示光标拖手和背景颜色（handle）
-//            needShowCursorHandle.value = newState.selection.start != newState.selection.end
-//
-//            // no change
-//            if (lastTextField == newState) return@BasicTextField
-//
-//            // used for some checks
-//            val textChanged = lastTextField.text.length != newState.text.length || lastTextField.text != newState.text
-//
-//            // scroll if invisible
-//            // when input some chars but target line invisible, will scroll to that line
-//            if(textChanged) {
-//                scrollIfInvisible()
-//            }
-//
-//            if (newState.text.contains('\n')) {
-//                onContainNewLine(newState)
-//            } else {
-//                // only update state when no linebreak,
-//                //   if has line break, still update, will got unexpected line break,
-//                //   for avoid it, just wait the text field list updated it, nothing need to do here
-//
-//                val newState = if(textChanged) {
-//                    newState
-//                } else {
-//                    // copy to avoid lost highlighting styles when text no changes,
-//                    // if styles still lost, try use `textFieldState.value` as `lastTextField`
-//                    lastTextField.copy(selection = newState.selection)
-//                }
-//
-//                currentTextField.value = newState
-//
-//                onUpdateText(newState, textChanged)
-//            }
-//        },
+
         //字体样式:字体颜色、字体大小、背景颜色等
         textStyle = textStyle.copy(
             fontSize = fontSize.sp,
